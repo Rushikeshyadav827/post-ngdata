adff510f6ed732b21c65e2d5e8bc489517dc2ed0
Rushikeshyadav827 <rushikeshsyadav827@gmail.com>
2022-05-19 15:41:43 +0530

    inital commint

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
